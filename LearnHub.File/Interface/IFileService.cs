d540fc1544cd2d58b44697dda3f14b434d547af6
kasra <kasramasra911@gmail.com>
2024-03-03 11:47:29 +0330

    File Service

---
+++ LearnHub.File/Interface/IFileService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace LearnHub.File.Interface
+{ 
+    public interface IFileService
+    {
+        public Tuple<int, string> SaveImage(IFormFile imageFile);
+        public bool DeleteImage(string imageFileName);
+    }
+}
